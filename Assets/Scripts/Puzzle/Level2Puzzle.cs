e2027b2bb164e895c09cdb8ae0c57d142378cbbe
Mukund-Agrawal0906 <mukund.agrawal22b@iiitg.ac.in>
2025-01-09 19:17:40 +0530

    tag commit

--- Assets/Scripts/Puzzle/Level2Puzzle.cs
+++ Assets/Scripts/Puzzle/Level2Puzzle.cs
@@ -1,28 +1,32 @@
 using UnityEngine;
 
-public class Level2Puzzle : MonoBehaviour
+namespace Puzzle
 {
-    public GameObject wall;
-    public float lowerSpeed = 2f;
-    public float targetYPosition = -3f;
-
-    private bool isTriggered = false;
-
-    void OnCollisionEnter(Collision other)
+    public class Level2Puzzle : MonoBehaviour
     {
-        if (other.gameObject.CompareTag("Pendant"))
+        public GameObject wall;
+        public float lowerSpeed = 2f;
+        public float targetYPosition = -3f;
+
+        private bool isTriggered = false;
+
+        void OnCollisionEnter(Collision other)
         {
-            isTriggered = true;
+            if (other.gameObject.CompareTag("Pendant"))
+            {
+                isTriggered = true;
+            }
         }
-    }
 
-    void Update()
-    {
-        if (isTriggered && wall.transform.position.y > targetYPosition)
+        void Update()
         {
-            Vector3 newPosition = wall.transform.position;
-            newPosition.y = Mathf.Max(newPosition.y - lowerSpeed * Time.deltaTime, targetYPosition);
-            wall.transform.position = newPosition;
+            if (isTriggered && wall.transform.position.y > targetYPosition)
+            {
+                Vector3 newPosition = wall.transform.position;
+                newPosition.y = Mathf.Max(newPosition.y - lowerSpeed * Time.deltaTime, targetYPosition);
+                wall.transform.position = newPosition;
+            }
         }
     }
 }
+

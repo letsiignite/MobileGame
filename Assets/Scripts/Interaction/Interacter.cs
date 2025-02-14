d3fba277722a9d70b3443c3196d85fa322a3f06b
Gaurav_3011 <gauravpitu0@gmail.com>
2025-02-14 11:19:52 +0530

    testing and fixing
    villager not on navmesh fixed
    added mainmenu in child bedroom in the night scene

--- Assets/Scripts/Interaction/Interacter.cs
+++ Assets/Scripts/Interaction/Interacter.cs
@@ -1,4 +1,4 @@
-//Gaurav's Code (new Input system try not functional)
+//Gaurav's Code (new Input system try, not functional)
 //Modified by Shrey (now functional fully using new input system)
 using Interactable;
 using UnityEngine;
